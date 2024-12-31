2ce068e94462d375164c3403f5cbb4057ab338f3
foxxcn <foxxcnweb@gmail.com>
2025-01-01 01:41:18 +0800

    feat!: Android apk build support

--- crates/egui_demo_app/src/lib.rs
+++ crates/egui_demo_app/src/lib.rs
@@ -6,6 +6,9 @@ mod backend_panel;
 mod frame_history;
 mod wrap_app;
 
+#[cfg(target_os = "android")]
+use winit::platform::android::activity::AndroidApp;
+
 pub use wrap_app::WrapApp;
 
 /// Time of day as seconds since midnight. Used for clock in demo app.
@@ -22,3 +25,31 @@ mod web;
 
 #[cfg(target_arch = "wasm32")]
 pub use web::*;
+
+// ----------------------------------------------------------------------------
+// Android entry point
+#[cfg(target_os = "android")]
+#[no_mangle]
+#[allow(unsafe_code)]
+pub fn android_main(app: AndroidApp) {
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_max_level(log::LevelFilter::Info)
+            .with_tag("egui_demo_app"),
+    );
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1280.0, 1024.0])
+            .with_drag_and_drop(true),
+        android_app: Some(app),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "egui demo app",
+        options,
+        Box::new(|cc| Ok(Box::new(crate::WrapApp::new(cc)))),
+    )
+    .unwrap();
+}
